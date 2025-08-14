5599d5e254d85b71dee90e8849491b894cdbed1b
Ashish Patel <99547279+ashish84k@users.noreply.github.com>
2025-08-14 20:13:54 +0530

    Update dbConnected.js

--- Divin-tasks/ExpressJs/database/dbConnected.js
+++ Divin-tasks/ExpressJs/database/dbConnected.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const DbConnected = () => {
     try {
-        mongoose.connect("mongodb://127.0.0.1:27017/myDatabase")
+        mongoose.connect("mongodb+srv://ashish84k:HackerLover2@cluster0.mongodb.net/TodoList?retryWrites=true&w=majority")
         .then(() => {
             console.log("MongoDB connected successfully");
         })
